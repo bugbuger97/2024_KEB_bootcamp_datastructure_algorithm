046801da07b01e2dabf6b0982ccfaac4c55ae116
bugbuger97 <junopaeng@gmail.com>
2024-02-08 11:25:30 +0900

    v0.2 mymath function set

--- mymath.py
+++ mymath.py
@@ -1,6 +1,21 @@
+import time
+def time_taken(func) -> float:
+    '''
+    print time taken
+    :param func: fuction
+    :return: time taken
+    '''
+    def wrapper(*args,**kwargs) -> None:
+        start = time.time()
+        result = func(*args,**kwargs)
+        end = time.time()
+        print(f"time elaped : {end-start}")
+        return result
+    return wrapper
 def factorial(number) -> int:
     """
     factorial function by recursion
+    use stack memory
     :param number: number
     :return: number!
     """
@@ -8,7 +23,7 @@ def factorial(number) -> int:
         return 1
     else:
         return number * factorial(number-1)
-
+@time_taken
 def nCr(n,r) -> int:
     '''
     combination function
