c49cd392e3cb4690acff7446f8dc8a71bae8d553
bugbuger97 <junopaeng@gmail.com>
2024-02-08 13:50:08 +0900

    v0.7 random game

--- day1.py
+++ day1.py
@@ -1,8 +1,18 @@
-import mymath
+import random
 if __name__ == "__main__":
-    n = int(input("Input n : "))
-    r = int(input("Input r : "))
-    print(f"{n}C{r} = {mymath.nCr(n,r)}\n")
-
-    # 시간 복잡도(Time complexity) : operation이 일어난 횟수 측정
-    # big-O natation == worst time case
+    result = random.randint(1,100)
+    cnt = 0
+    opportunities = 7
+    while cnt < opportunities:
+        cnt += 1
+        response = int(input("Input number : "))
+        if result > response:
+            print("up")
+        elif result < response:
+            print("down")
+        else:
+            print("correct!!")
+            print(f"{cnt}번만에 정답 번호 : {result}인 것을 맞추었습니다.")
+            break
+    else:
+        print("failed!! You lose......")
