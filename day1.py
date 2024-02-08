a56b91e6a16ff96033ecf21413d56f65e6ecb15d
bugbuger97 <junopaeng@gmail.com>
2024-02-08 13:25:50 +0900

    v0.6 ocp

--- day1.py
+++ day1.py
@@ -1,6 +1,8 @@
 import mymath
-import time
 if __name__ == "__main__":
     n = int(input("Input n : "))
     r = int(input("Input r : "))
     print(f"{n}C{r} = {mymath.nCr(n,r)}\n")
+
+    # 시간 복잡도(Time complexity) : operation이 일어난 횟수 측정
+    # big-O natation == worst time case
