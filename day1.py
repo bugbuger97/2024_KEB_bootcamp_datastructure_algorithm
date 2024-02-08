7c589977fa57572c7ccb7c99c738e5c8e4be43a6
bugbuger97 <junopaeng@gmail.com>
2024-02-08 11:25:06 +0900

    v0.5 ocp

--- day1.py
+++ day1.py
@@ -3,7 +3,4 @@ import time
 if __name__ == "__main__":
     n = int(input("Input n : "))
     r = int(input("Input r : "))
-    start = time.time()
     print(f"{n}C{r} = {mymath.nCr(n,r)}\n")
-    end = time.time()
-    print(f"소요 시간 : {end-start}")
