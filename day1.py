227bbc48d0b21dcfbeeba1dc52bbb707eaaeb0df
bugbuger97 <junopaeng@gmail.com>
2024-02-08 11:12:30 +0900

    v0.4 time taken

--- day1.py
+++ day1.py
@@ -1,26 +1,9 @@
-def factorial(number) -> int:
-    """
-    factorial function by recursion
-    :param number: number
-    :return: number!
-    """
-    if number <= 1:
-        return 1
-    else:
-        return number * factorial(number-1)
-
-def nCr(n,r) -> int:
-    '''
-    combination function
-    :param n: n
-    :param r: r
-    :return: nCr
-    '''
-    numerator = factorial(n)
-    denominator = factorial(n-r) * factorial(r)
-    return int(numerator / denominator)
-
+import mymath
+import time
 if __name__ == "__main__":
     n = int(input("Input n : "))
     r = int(input("Input r : "))
-    print(f"{n}C{r} = {nCr(n,r)}\n")
+    start = time.time()
+    print(f"{n}C{r} = {mymath.nCr(n,r)}\n")
+    end = time.time()
+    print(f"소요 시간 : {end-start}")
