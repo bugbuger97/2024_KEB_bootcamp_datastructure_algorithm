57b0bee2dc92dea8d8d20b7205cb7fe0acf8f71d
bugbuger97 <junopaeng@gmail.com>
2024-02-13 14:36:50 +0900

    Stack branket v0.2

--- day2.py
+++ day2.py
@@ -1,39 +1,20 @@
 # Stack
-def push(data):
-	global SIZE, stack, top
-	if top >= SIZE-1:
-		print("Stack is full")
-		return
-	else:
-		top += 1
-		stack[top] = data
+def check_bracket(expr: str) -> bool:
+    """
+    check bracket in expression.
+    :param expr: str
+    :return: bool
+    """
+    stack = list()
+    table = {')': '(', ']': '[', '}': '{', '>': '<'}
+    for char in expr:
+        if char not in table:
+            stack.append(char)
+        elif not stack or table[char] != stack.pop():
+             return False
+        else:
+            pass
+    return len(stack) == 0
 
-def pop():
-	global SIZE, stack, top
-	if top < 0:
-		print("Stack is empty")
-	else:
-		print(stack[top])
-		stack[top] = None
-		top -= 1
-	return
-
-SIZE = 5
-stack = ["0", "1", "2", "3", None]
-top = 3
 if __name__ == "__main__":
-	print(stack)
-	push("4")
-	print(stack)
-	push("5")
-	pop()
-	print(stack)
-	pop()
-	print(stack)
-	pop()
-	print(stack)
-	pop()
-	print(stack)
-	pop()
-	print(stack)
-	pop()
+    print(check_bracket(input("Input expression : ")))
