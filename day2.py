234c6ccf3475eefd76761bf3b10aaac8adffbff7
bugbuger97 <junopaeng@gmail.com>
2024-02-13 16:19:16 +0900

    Stack branket v0.3

--- day2.py
+++ day2.py
@@ -1,4 +1,15 @@
-# Stack
+# Stack : First In Last Out
+# Queue : First In First Out
+
+# Linear Queue(데이터가 적을 때, 사용)
+# enQueue : rear가 증가
+# deQueue : front가 증가
+
+# 원형 큐(데이터가 많을 때, 사용)
+# 원형 큐가 가득 찼을 때 조건 : (rear값 + 1) % 큐의 SIZE == front값
+# enQueue : rear = (rear + 1) % 큐의 SIZE
+# deQueue : front = (front + 1) % SIZE
+
 def check_bracket(expr: str) -> bool:
     """
     check bracket in expression.
@@ -8,13 +19,15 @@ def check_bracket(expr: str) -> bool:
     stack = list()
     table = {')': '(', ']': '[', '}': '{', '>': '<'}
     for char in expr:
-        if char not in table:
+        # if char not in table:
+        if char in table.values():  # "([{<"
             stack.append(char)
-        elif not stack or table[char] != stack.pop():
-             return False
-        else:
-            pass
+        elif char in table.keys():  # ")]}>"
+            if not stack or table[char] != stack.pop():
+                return False
     return len(stack) == 0
 
+
 if __name__ == "__main__":
-    print(check_bracket(input("Input expression : ")))
+    expression = input("Input expression : ")
+    print(check_bracket(expression))
