557379d55338d2b6a0dd4a84e374ee80951e945e
bugbuger97 <junopaeng@gmail.com>
2024-02-14 14:15:18 +0900

    dfs v0.1

--- day2.py
+++ day2.py
@@ -1,33 +1,24 @@
-# Stack : First In Last Out
-# Queue : First In First Out
+# 재귀 함수로 구현함.
+# preorder traversal
+# inorder traversal
+# postorder traversal
 
-# Linear Queue(데이터가 적을 때, 사용)
-# enQueue : rear가 증가
-# deQueue : front가 증가
+# 깊이 우선 탐색
+def dfs(g, v, visited):
+    visited[v] = 1
+    print(chr(ord('A')+v), end=' ')
+    for i in range(len(g)):
+        if g[v][i] == True and not visited[i]:
+            dfs(g,i,visited)
 
-# 원형 큐(데이터가 많을 때, 사용)
-# 원형 큐가 가득 찼을 때 조건 : (rear값 + 1) % 큐의 SIZE == front값
-# enQueue : rear = (rear + 1) % 큐의 SIZE
-# deQueue : front = (front + 1) % SIZE
-
-def check_bracket(expr: str) -> bool:
-    """
-    check bracket in expression.
-    :param expr: str
-    :return: bool
-    """
-    stack = list()
-    table = {')': '(', ']': '[', '}': '{', '>': '<'}
-    for char in expr:
-        # if char not in table:
-        if char in table.values():  # "([{<"
-            stack.append(char)
-        elif char in table.keys():  # ")]}>"
-            if not stack or table[char] != stack.pop():
-                return False
-    return len(stack) == 0
-
+graph = [
+    [0,1,0,1,0],
+    [1,0,1,1,0],
+    [0,1,0,0,1],
+    [1,1,0,0,1],
+    [0,0,1,1,0]
+]
+visited = [False] * len(graph)
 
 if __name__ == "__main__":
-    expression = input("Input expression : ")
-    print(check_bracket(expression))
+    dfs(graph,0,visited)
