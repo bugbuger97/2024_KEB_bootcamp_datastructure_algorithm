828c37c96425caa6fff35fe9ca52caa6cedf4b13
bugbuger97 <junopaeng@gmail.com>
2024-02-14 14:42:52 +0900

    dfs v0.3

--- day3.py
+++ day3.py
@@ -2,16 +2,17 @@ def dfs(g, i, visited):
     visited[i] = 1
     print(chr(ord('A')+i), end=' ')
     for j in range(len(g)):
-        if g[i][j] == True and not visited[j]:
-            dfs(g,j,visited)
+        if g[i][j] == 1 and not visited[j]:
+            dfs(g, j, visited)
 
 graph = [
-    [0,0,1,1,0],
-    [0,0,1,0,0],
-    [1,1,0,1,1],
-    [1,0,1,0,0],
-    [0,0,1,0,0]
+    [0, 1, 1, 0, 0, 0],
+    [1, 0, 0, 1, 0, 0],
+    [1, 0, 0, 1, 0, 0],
+    [0, 1, 1, 0, 1, 1],
+    [0, 0, 0, 1, 0, 1],
+    [0, 0, 0, 1, 1, 0]
 ]
-visited = [False] * len(graph)
-if __name__ == "__main__":
-    dfs(graph,0,visited)
+
+visited = [0 for _ in range(len(graph))]
+dfs(graph, 0, visited)
