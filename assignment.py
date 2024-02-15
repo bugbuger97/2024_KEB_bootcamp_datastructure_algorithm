36055551e8d36809508ef91cf6b4e35a39392ec2
bugbuger97 <junopaeng@gmail.com>
2024-02-15 12:31:11 +0900

    Linked List 4-1

--- assignment.py
+++ assignment.py
@@ -1,49 +1,54 @@
-"""
-Linked list
-[data, link] -> [data, link] -> [data, link]
-노드들이 물리적으로 떨어져 있지만 link로 이어져 있어 마치 선형 리스트와 비슷하게 이어져 있음.
-
-node insert
-1. 삽입할 새 노드 생성
-2. 새 노드의 링크에 다음에 들어갈 node를 연결 시킴
-3. 전 node의 링크를 새 노드에 연결 시킴
-
-node delete
-1. 삭제할 노드의 전 노드의 링크를 삭제할 노드의 후 노드에 바로 연결시킴
-2. 삭제할 노드를 삭제 시킴
-"""
-
-# 4-1
-class Node():
-    def __init__(self,data=None):
-        self.data = data
-        self.link = None
-    def Link(self,link):
-        self.link = link
-def print_linked_list():
-    current = head
-    if current == None:
+class Node() :
+	def __init__ (self) :
+		self.data = None
+		self.link = None
+def print_Nodes(start) :
+	current = start
+	if current == None :
+		return
+	print(current.data, end = ' ')
+	while current.link != None:
+		current = current.link
+		print(current.data, end = ' ')
+	print()
+def delete_Node(deleteData):
+    global head, current, pre
+    if head.data == deleteData:
+        current = head
+        head = head.link
+        del(current)
+        print("첫 노드가 삭제됨")
         return
-    print(current.data, end=" -> ")
-    while current.link is not None:
+    current = head
+    while current.link != None :
+        pre = current
         current = current.link
-        print(current.data, end = " -> ")
-    print(current.link)
+        if current.data == deleteData :
+            pre.link = current.link
+            del(current)
+            print("중간 노드가 삭제됨")
+            return
+    else:
+        print("삭제된 노드가 없음")
 
-head = Node('head')
+head, current, pre = None, None, None
+dataArray = ["가", "나", "다", "라", "마"]
+
 if __name__ == "__main__":
-    node1 = Node("가")
-    node2 = Node("나")
-    node3 = Node("다")
-    node4 = Node("라")
-    node5 = Node("마")
-    node6 = Node("바")
-    node7 = Node("사")
-    head.Link(node1)
-    node1.Link(node2)
-    node2.Link(node3)
-    node3.Link(node4)
-    node4.Link(node5)
-    node5.Link(node6)
-    node6.Link(node7)
-    print_linked_list()
+    node = Node()
+    node.data = dataArray[0]
+    head = node
+    for data in dataArray[1:]:
+        pre = node
+        node = Node()
+        node.data = data
+        pre.link = node
+    print_Nodes(head)
+    delete_Node("가")
+    print_Nodes(head)
+    delete_Node("다")
+    print_Nodes(head)
+    delete_Node("라")
+    print_Nodes(head)
+    delete_Node("라")
+    print_Nodes(head)
