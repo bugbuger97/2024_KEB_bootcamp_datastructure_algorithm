7aa55fd4240e1b800016b2aa5b76617a9527c329
bugbuger97 <junopaeng@gmail.com>
2024-02-16 11:37:39 +0900

    Stack Application example 2

--- assignment.py
+++ assignment.py
@@ -6,18 +6,21 @@ class Stack:
 	def push(self, data):
 		self.stack.append(data)
 	def pop(self):
-		print(self.stack[-1], end = "->")
+		print(self.stack[-1],end="")
 		del(self.stack[-1])
 
 if __name__ == "__main__":
-	print("과자집에 가는 길 : ",end="")
-	color = input().split("->")
-	if color[-1] == "과자집":
-		del(color[-1])
+	string = """
+	진달래꽃
+	나 보기가 역겨워
+	가실 때에는
+	말 없이 고이 보내드리오리다.
+	"""
+	print("----------- 원본 -----------")
+	print(string)
+	print("----------- 결과 -----------")
 	s = Stack()
-	for i in color:
+	for i in string:
 		s.push(i)
-	print("우리집에 오는 길 : ", end="")
 	for i in range(len(s.stack)):
 		s.pop()
-	print("우리집")
