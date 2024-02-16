86afb7987cb790611948a7bec4ff686d0c3184ff
bugbuger97 <junopaeng@gmail.com>
2024-02-16 11:31:11 +0900

    Stack Application example 1

--- assignment.py
+++ assignment.py
@@ -1,52 +1,23 @@
-# double linked list 응용 2
-import random
-class Node():
-	def __init__(self,data=None):
-		self.left = None
-		self.data = data
-		self.right = None
-def print_right_Node(start):
-	current = start
-	if current.right == None:
-		return
-	print(current.data, end=" ")
-	while current.right != None:
-		current = current.right
-		print(current.data, end = " ")
-	print()
-def print_left_Node(start):
-	current = start
-	if current.right == None:
-		return
-	while current.right != None:
-		current = current.right
-	print(current.data, end=" ")
-	while current.left != None:
-		current = current.left
-		print(current.data, end = " ")
-	print()
-def make_double_linked_list(data):
-	global head
-	node = Node(data)
-	if head == None:
-		head = node
-		return
-	if head.right == None and head.left == None:
-		head.right = node
-		node.left = head
-		return
-	current = head
-	while current.right != None:
-		current = current.right
-		if current.right == None:
-			current.right = node
-			node.left = current
-			return
+# Stack 응용 1
 
-head = None
-arr = [chr(i+65) for i in range(5)]
+class Stack:
+	def __init__(self):
+		self.stack = list()
+	def push(self, data):
+		self.stack.append(data)
+	def pop(self):
+		print(self.stack[-1], end = "->")
+		del(self.stack[-1])
+
 if __name__ == "__main__":
-	for i in arr:
-		make_double_linked_list(i)
-	print_right_Node(head)
-	print_left_Node(head)
+	print("과자집에 가는 길 : ",end="")
+	color = input().split("->")
+	if color[-1] == "과자집":
+		del(color[-1])
+	s = Stack()
+	for i in color:
+		s.push(i)
+	print("우리집에 오는 길 : ", end="")
+	for i in range(len(s.stack)):
+		s.pop()
+	print("우리집")
