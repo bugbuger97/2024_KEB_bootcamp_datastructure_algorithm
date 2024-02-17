ba6d64821ac93b024237c930fa4f3a1ff2ef4ef4
bugbuger97 <junopaeng@gmail.com>
2024-02-17 17:02:01 +0900

    Circular Queue Application example 2

--- assignment.py
+++ assignment.py
@@ -2,7 +2,6 @@
 Circular Queue
 FIFO의 원칙을 유지하면서 큐의 입구와 출구를 연결한 구조이다.
 '''
-
 def is_Queue_full():
 	global size, queue, front, rear
 	if (rear + 1) % size == front:
@@ -38,36 +37,20 @@ def peek():
 		return None
 	return queue[(front+1)%size]
 
-size = 5
+size = 6
 queue = [None for _ in range(size)]
 front = rear = 0
 if __name__ == "__main__":
-	print(queue)
-	print(f"front = {front}, rear = {rear}")
-	enQueue("a")
-	print(f"front = {front}, rear = {rear}")
-	print(queue)
-	enQueue("b")
-	print(f"front = {front}, rear = {rear}")
-	print(queue)
-	enQueue("c")
-	print(f"front = {front}, rear = {rear}")
-	print(queue)
-	enQueue("d")
-	print(f"front = {front}, rear = {rear}")
-	print(queue)
-	enQueue("e")
-	print(f"front = {front}, rear = {rear}")
-	print(queue)
-	deQueue()
-	print(queue)
-	print(f"front = {front}, rear = {rear}")
-	deQueue()
-	print(queue)
-	print(f"front = {front}, rear = {rear}")
-	deQueue()
-	print(queue)
-	print(f"front = {front}, rear = {rear}")
-	enQueue("x")
-	print(queue)
-	print(f"front = {front}, rear = {rear}")
+	Sum = 0
+	temp = [("사용",9),("고장",3),("환불",4),("환불",4),("고장",3)]
+	print(f'귀하의 대기 예상시간은 0 분입니다.')
+	print(f'현재 대기 콜 --> {queue}\n')
+	for data in temp:
+		enQueue(data)
+		if is_Queue_full():
+			print(f'최종 대기 콜 --> {queue}')
+			print('프로그램 종료\n')
+			break
+		Sum += queue[rear][1]
+		print(f'귀하의 대기 예상시간은 {Sum} 분입니다.')
+		print(f'현재 대기 콜 --> {queue}\n')
